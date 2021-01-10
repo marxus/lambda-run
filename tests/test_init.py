0ede5c0208097d24179e27c865ee9c6cf9529794
Amit Marcus <marxus@gmail.com>
2021-01-10 23:38:18 +0200

    added cli

---
+++ tests/test_init.py
@@ -0,0 +1,30 @@
+from lambda_run import wrap_handler
+
+py = lambda payload: {'lambdaRun': ['py', payload]}
+sh = lambda payload: {'lambdaRun': ['sh', payload]}
+
+lambda_handler = wrap_handler(lambda ev, ctx: 'OK')
+
+
+def test_event():
+    rsp = lambda_handler({'some': 'event'}, None)
+    assert rsp == 'OK'
+
+
+def test_run_py():
+    rsp = lambda_handler(py("""
+print(123)
+print('abc')
+print("xyz")
+"""), None)
+    assert rsp == (0, '123\nabc\nxyz')
+
+
+def test_run_sh():
+    rsp = lambda_handler(sh("""
+ls -1
+ls -1 /
+"""), None)
+    stdout = rsp[1]
+    assert 'setup.py' in stdout
+    assert 'root' in stdout
